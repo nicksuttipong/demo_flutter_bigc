0fbc42b80485a537c0f3496c7ab2297615449a33
Suttipong Runaglitt <suttipong.rua@bigc.co.th>
2021-05-27 15:01:34 +0700

    get data

--- lib/states/service_user.dart
+++ lib/states/service_user.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
+import 'package:bigcproj/model/product_model.dart';
 import 'package:bigcproj/utilities/constant/con_colors.dart';
+import 'package:bigcproj/utilities/style/style_text.dart';
+import 'package:bigcproj/widgets/show_progress.dart';
+import 'package:bigcproj/widgets/show_title.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -15,9 +22,28 @@ class _ServiceUserState extends State<ServiceUser> {
     'assets/images/b2.png',
     'assets/images/b3.png',
   ];
+  List<ProductModel> productModels = [];
 
   void initState() {
     super.initState();
+    buildWidget();
+    readData();
+  }
+
+  Future<Null> readData() async {
+    String apiFoods = 'https://www.androidthai.in.th/bigc/getAllFood.php';
+    await Dio().get(apiFoods).then((value) {
+      var result = json.decode(value.data);
+      for (var item in result) {
+        ProductModel model = ProductModel.fromMap(item);
+        setState(() {
+          productModels.add(model);
+        });
+      }
+    });
+  }
+
+  void buildWidget() {
     for (var item in pathImages) {
       widgets.add(Image.asset(item));
     }
@@ -33,24 +59,47 @@ class _ServiceUserState extends State<ServiceUser> {
           buildSignOut(),
         ],
       ),
-      body: Column(
-        children: [
-          buildBanners(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            buildBanners(),
+            buildTitle(),
+            productModels.length == 0
+                ? ShowProgress()
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: ScrollPhysics(),
+                    itemCount: productModels.length,
+                    itemBuilder: (context, index) =>
+                        Text(productModels[index].nameFood),
+                  ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Row buildTitle() {
+    return Row(
+      children: [
+        SizedBox(
+          width: 16,
+        ),
+        ShowTitle(title: 'Product: ', textStyle: StyleText().h1Style()),
+      ],
     );
   }
 
   CarouselSlider buildBanners() {
     return CarouselSlider(
-          items: widgets,
-          options: CarouselOptions(
-            autoPlay: true,
-            autoPlayAnimationDuration: Duration(seconds: 2),
-            aspectRatio: 16/9,
-            enlargeCenterPage: true,
-          ),
-        );
+      items: widgets,
+      options: CarouselOptions(
+        autoPlay: true,
+        autoPlayAnimationDuration: Duration(seconds: 2),
+        aspectRatio: 16 / 9,
+        enlargeCenterPage: true,
+      ),
+    );
   }
 
   IconButton buildSignOut() {
