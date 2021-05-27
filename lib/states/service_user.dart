680ecd03e3583e281e747c384059c2aceb05a485
Suttipong Runaglitt <suttipong.rua@bigc.co.th>
2021-05-27 14:21:30 +0700

    add carousel banner

--- lib/states/service_user.dart
+++ lib/states/service_user.dart
@@ -1,4 +1,5 @@
 import 'package:bigcproj/utilities/constant/con_colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,6 +9,20 @@ class ServiceUser extends StatefulWidget {
 }
 
 class _ServiceUserState extends State<ServiceUser> {
+  List<Widget> widgets = [];
+  List<String> pathImages = [
+    'assets/images/b1.png',
+    'assets/images/b2.png',
+    'assets/images/b3.png',
+  ];
+
+  void initState() {
+    super.initState();
+    for (var item in pathImages) {
+      widgets.add(Image.asset(item));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,17 +33,34 @@ class _ServiceUserState extends State<ServiceUser> {
           buildSignOut(),
         ],
       ),
+      body: Column(
+        children: [
+          buildBanners(),
+        ],
+      ),
     );
+  }
+
+  CarouselSlider buildBanners() {
+    return CarouselSlider(
+          items: widgets,
+          options: CarouselOptions(
+            autoPlay: true,
+            autoPlayAnimationDuration: Duration(seconds: 2),
+            aspectRatio: 16/9,
+            enlargeCenterPage: true,
+          ),
+        );
   }
 
   IconButton buildSignOut() {
     return IconButton(
-          onPressed: () async {
-            SharedPreferences preference = await SharedPreferences.getInstance();
-            preference.clear();
-            Navigator.pushNamedAndRemoveUntil(context, '/authen', (route) => false);
-          },
-          icon: Icon(Icons.exit_to_app),
-        );
+      onPressed: () async {
+        SharedPreferences preference = await SharedPreferences.getInstance();
+        preference.clear();
+        Navigator.pushNamedAndRemoveUntil(context, '/authen', (route) => false);
+      },
+      icon: Icon(Icons.exit_to_app),
+    );
   }
 }
